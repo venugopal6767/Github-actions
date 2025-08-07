1945870c4fb82b2502038d1bb3504b2014e3ffcc
venu67776 <83317608+venu67776@users.noreply.github.com>
2025-08-07 16:47:38 +0530

    new

--- nodejs-app/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello from Node.js!'));
-app.listen(3000, () => console.log('Node app running on port 3000'));
